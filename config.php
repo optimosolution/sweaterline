c92104643a8e5973dc12b6528c2d911f70962900
Saidur Rahman <saidurwd@gmail.com>
2017-04-11 22:23:25 +0600

    First time upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/sweaterline/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/sweaterline/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/xampp/htdocs/sweaterline/catalog/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/sweaterline/system/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/sweaterline/image/');
+define('DIR_LANGUAGE', 'D:/xampp/htdocs/sweaterline/catalog/language/');
+define('DIR_TEMPLATE', 'D:/xampp/htdocs/sweaterline/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/xampp/htdocs/sweaterline/system/config/');
+define('DIR_CACHE', 'D:/xampp/htdocs/sweaterline/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/xampp/htdocs/sweaterline/system/storage/download/');
+define('DIR_LOGS', 'D:/xampp/htdocs/sweaterline/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/xampp/htdocs/sweaterline/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/xampp/htdocs/sweaterline/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'sweaterline');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'sl_');
